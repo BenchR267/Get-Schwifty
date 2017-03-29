3cc1f6b9c709bf129614710744085e8b428bb3ea
Benjamin Herzog <mail@benchr.de>
2017-03-29 15:27:14 +0200

    Initial Commit

---
+++ WWDC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WWDC
+//
+//  Created by Benjamin Herzog on 29.03.17.
+//  Copyright © 2017 Benjamin Herzog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
