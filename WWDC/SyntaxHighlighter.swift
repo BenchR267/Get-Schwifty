7d8c6adc400f692b51166d4d01a0fa772633969a
Benjamin Herzog <mail@benchr.de>
2017-04-01 09:28:27 +0200

    Added basic syntax highlighter and fixed some lexer issues

---
+++ WWDC/SyntaxHighlighter.swift
@@ -0,0 +1,44 @@
+//
+//  SyntaxHighlighter.swift
+//  WWDC
+//
+//  Created by Benjamin Herzog on 01.04.17.
+//  Copyright © 2017 Benjamin Herzog. All rights reserved.
+//
+
+import UIKit
+
+func attributedString(tokens: [Token]) -> NSAttributedString {
+    let attr = NSMutableAttributedString(string: "")
+    
+    for t in tokens {
+        let color: UIColor
+        switch t.type {
+        case .keyword:              color = keyword
+        case .comment(_):           color = comments
+        case .literal(.String(_)):  color = strings
+        case .literal(.Integer(_)): color = numbers
+        default:                    color = standard
+        }
+        attr.append(NSAttributedString(string: t.raw, attributes: [
+            NSFontAttributeName: font,
+            NSForegroundColorAttributeName: color
+        ]))
+    }
+    
+    return attr
+}
+
+extension UIColor {
+    convenience init(r: Int, g: Int, b: Int, a: Double) {
+        self.init(red: CGFloat(r)/255, green: CGFloat(g)/255, blue: CGFloat(b)/255, alpha: CGFloat(a))
+    }
+}
+
+private let font = UIFont.systemFont(ofSize: 14)
+
+private let standard = UIColor(r: 225, g: 226, b: 231, a: 1)
+private let keyword = UIColor(r: 225, g: 44, b: 160, a: 1)
+private let comments = UIColor(r: 69, g: 187, b: 62, a: 1)
+private let strings = UIColor(r: 211, g: 35, b: 46, a: 1)
+private let numbers = UIColor(r: 20, g: 156, b: 146, a: 1)
--- WWDC/ViewController.swift
+++ WWDC/ViewController.swift
@@ -10,18 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    lazy var input: String = {
         guard let path = Bundle.main.path(forResource: "test", ofType: "txt") else {
             fatalError("Could not find test.txt in Bundle!")
         }
         
-        let input = (try? String(contentsOfFile: path)) ?? ""
+        return (try? String(contentsOfFile: path)) ?? ""
+    }()
+    
+    var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        let lexer = Lexer(input: input)
+        self.textView = UITextView(frame: self.view.bounds)
+        self.textView.backgroundColor = UIColor(colorLiteralRed: 31.0/255, green: 32.0/255, blue: 41.0/255, alpha: 1)
+        
+        self.textView.text = self.input
+        self.textView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.view.addSubview(self.textView)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let lexer = Lexer(input: self.input)
         let tokens = lexer.start()
         dump(tokens)
+        self.textView.attributedText = attributedString(tokens: tokens)
     }
 
 }
